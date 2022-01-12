4a672e7a664a931cbcf3a0aed288d83ffe81a95e
smallchen2016 <zha@smallchen.com>
2022-01-12 12:24:55 +0800

    刚弄完就火爆了

---
+++ JDog202201/Entity/HomeEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDog202201.Entity
+{
+    public class HomeEntity
+    {
+        public class homeMainInfo
+        {
+            public int curCity{ set; get; }
+            public bool dropHongbaoDisplay { set; get; }
+            public int jdCipher{ set; get; }
+            public int pkJdCipher { set; get; }
+            public string secretp{ set; get; }
+            public int shareMiniprogramSwitch{ set; get; }
+            public int taskSwitch{ set; get; }
+            public int todaySignStatus{ set; get; }
+            public int totalCity{ set; get; }
+            public int userType { set; get; }
+        }
+
+        public class userMainInfo
+        {
+            public int now_score { set; get; }
+            public int next_score { set; get; }
+            public int bonus { set; get; }
+            public int now_city { set; get; }
+            public int all_city { set; get; }
+        }
+
+        public homeMainInfo homeMain { set; get; }
+        public userMainInfo userMain { set; get; }
+        public string secretp { set; get; }
+
+    }
+}
